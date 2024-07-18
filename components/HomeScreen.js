41c18c4dc470f84999137e0a2e7d89430438a8aa
Lucas Matos <matoslucas@hotmail.com>
2024-07-18 15:47:10 -0600

    Update campaing

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { ScrollView, StyleSheet, ActivityIndicator } from "react-native";
 import {
-  ScrollView,
-  StyleSheet,
-  ActivityIndicator,
-} from "react-native";
-import { Avatar, Card, Title, Paragraph, Button } from "react-native-paper";
+  Avatar,
+  Card,
+  Title,
+  Paragraph,
+  Button,
+  Text,
+} from "react-native-paper";
 import carouselData from "../assets/carouselData.json";
-import halloween from "../assets/enhanced_halloween.png"; // Ensure this path is correct
+import halloween from "../assets/enhanced_halloween.png";
+import men from "../assets/men.webp";
 
 const HomeScreen = () => {
   const [data, setData] = useState([]);
@@ -37,22 +41,26 @@ const HomeScreen = () => {
       {data.map((item, index) => (
         <Card key={item.id} style={styles.card}>
           <Card.Cover
-            source={
-              index === 0 ? halloween : { uri: item.image }
-            } // Use enhanced Halloween image for the first card
+            source={index === 0 ? halloween : men} // Use enhanced Halloween image for the first card
             style={styles.image}
           />
           <Card.Title
             title={item.text}
-            left={(props) => <Avatar.Icon {...props} icon="city" />}
+            left={(props) => <Avatar.Icon {...props} icon="sale" style={{ backgroundColor: '#D32F2F' }}  />}
           />
           <Card.Content style={styles.cardContent}>
-            <Title>{item.title}</Title>
-            <Paragraph>{item.description}</Paragraph>
+            <Paragraph>{item.des}</Paragraph>
           </Card.Content>
           <Card.Actions style={styles.cardActions}>
-            <Button mode="contained" onPress={() => {}} style={styles.button} color="#6200ee">Share</Button>
-            <Button mode="contained" onPress={() => {}} style={styles.button} color="#6200ee">Explore</Button>
+            
+            <Button
+              mode="contained"
+              onPress={() => {}}
+              style={styles.button}
+              color="#D32F2F"
+            >
+              Get Dsicount
+            </Button>
           </Card.Actions>
         </Card>
       ))}
@@ -67,21 +75,21 @@ const styles = StyleSheet.create({
   card: {
     margin: 10,
     borderRadius: 8, // Rounded corners
-    overflow: 'hidden', // Ensures the image does not bleed outside the border radius
+    overflow: "hidden", // Ensures the image does not bleed outside the border radius
   },
   image: {
     height: 200,
-    width: '100%', // Ensures the image covers the full width of the card
+    width: "100%", // Ensures the image covers the full width of the card
   },
   cardContent: {
     padding: 10, // Consistent padding inside the card
   },
   cardActions: {
-    flexDirection: 'row',
-    justifyContent: 'space-around', // Spreads the buttons out evenly
+    flexDirection: "row",
+    justifyContent: "space-around", // Spreads the buttons out evenly
   },
   button: {
-    width: '40%', // Button width to make them larger and more tappable
+    width: "40%", // Button width to make them larger and more tappable
     marginHorizontal: 5, // Adds horizontal space between buttons
   },
 });
